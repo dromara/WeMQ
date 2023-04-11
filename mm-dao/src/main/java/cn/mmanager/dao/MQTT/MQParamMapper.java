4e354f9cce26a1ec6b5b136cd8e4a6a8d5e5bf3b
NicholasLD <878639947@qq.com>
2023-04-12 00:48:01 +0800

    调试参数DAO

---
+++ mm-dao/src/main/java/cn/mmanager/dao/MQTT/MQParamMapper.java
@@ -0,0 +1,39 @@
+package cn.mmanager.dao.MQTT;
+
+import cn.mmanager.model.pojo.MQParam;
+
+import java.util.List;
+
+/**
+ * @author 梁晓惠、袁祎阳、纪雨佳
+ * @createTime 2023/4/8 15:16
+ */
+public interface MQParamMapper {
+    /**
+     * 添加
+     * @param mqParam 参数实体类
+     * @return 返回值
+     */
+    int add(MQParam mqParam);
+
+    /**
+     * 通过id修改
+     * @param mqParam 参数实体类
+     * @return 返回值
+     */
+    int update(MQParam mqParam);
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    int deleteById(int id);
+
+    /**
+     * 查询
+     * @param id 参数id
+     * @return 返回值
+     */
+    List<MQParam> select (Long id);
+
+}
